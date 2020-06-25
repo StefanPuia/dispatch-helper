15035bad5d2f50f612b3827931d6dadbabeb2f9f
Stefan <stefan.puia13@gmail.com>
2020-06-25 18:19:30 +0100

    fixed wsserver log to show forwarded ip

--- server/src/wsserver.js
+++ server/src/wsserver.js
@@ -8,22 +8,22 @@ const initWebSocketServer = (server) => {
     });
 
     wsServer.on("request", function (request) {
+        const ip = request.httpRequest.headers["x-real-ip"] || request.remoteAddress;
+        const address = ip === "::1" ? "localhost" : ip;
         const connection = request.accept("echo-protocol", request.origin);
-        console.log(new Date() + " Connection accepted.");
+        console.log(new Date() + ` ${address} connection accepted.`);
         connections.push(connection);
         connection.on("close", function (reasonCode, description) {
             const index = connections.indexOf(connection);
             if (index > -1) {
                 connections.splice(index, 1);
             }
-            console.log(
-                new Date() + " Peer " + (connection.remoteAddress === "::1" ? "local" : connection.remoteAddress) + " disconnected."
-            );
+            console.log(new Date() + ` ${address} peer disconnected.`);
         });
     });
 };
 
 module.exports = {
     init: initWebSocketServer,
-    connections: connections
+    connections: connections,
 };
